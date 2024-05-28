3b4799c087d0bfb00f6b78e0a1e3cc2b4f39975f
dokma11 <vule.dok@gmail.com>
2024-05-28 17:30:59 +0200

    feat: Implement user DTOs

---
+++ NAIS-projekat/MuseumService/src/main/java/org/example/museumservice/dto/GuestResponseDTO.java
@@ -0,0 +1,12 @@
+package org.example.museumservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GuestResponseDTO extends UserResponseDTO {
+
+    private String biography;
+
+}
