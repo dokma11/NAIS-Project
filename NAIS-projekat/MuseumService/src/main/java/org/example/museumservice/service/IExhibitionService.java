c5d14677ab54d3b82b99731bb7729d94608f370d
kuzminacc <kuzminacn@gmail.com>
2024-05-28 17:27:58 +0200

    feat: adding exhibition

---
+++ NAIS-projekat/MuseumService/src/main/java/org/example/museumservice/service/IExhibitionService.java
@@ -0,0 +1,9 @@
+package org.example.museumservice.service;
+
+import java.util.List;
+
+import org.example.museumservice.model.Exhibition;
+
+public interface IExhibitionService extends ICRUDService<Exhibition, Integer> {
+    List<Exhibition> findAll();
+} 
